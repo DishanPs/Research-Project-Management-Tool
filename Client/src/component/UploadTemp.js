da8a53cf5054ae4d84741cdbee1179c002ab3184
RisinaR <rkarisna@gmail.com>
2022-05-30 21:54:21 +0530

    Resolved Frontend CSS errors

--- Client/src/component/UploadTemp.js
+++ Client/src/component/UploadTemp.js
@@ -7,100 +7,111 @@ import AdminSideNavBar from "./AdminSideNavBar";
 
 const UploadTemp = () => {
   return (
-    <div style={{backgroundImage: `url("https://i.pinimg.com/originals/58/54/23/58542340effa27e4941369bd76c70e15.jpg")`, backgroundSize: "cover", height: "110vh", marginLeft:"20vh"}}>
+    <div
+      style={{
+        backgroundImage: `url("https://i.pinimg.com/originals/58/54/23/58542340effa27e4941369bd76c70e15.jpg")`,
+        backgroundSize: "cover",
+        height: "110vh",
+      }}
+    >
       <AdminSideNavBar />
       <div
         style={{
           marginLeft: "200px",
         }}
       >
-      <center>
-        <h1>Templates</h1>
-      </center>
-      <hr />
-
-      <u>
-        <h3>Proposal Document</h3>
-      </u>
-      <br />
-      <Link to="/uploaddoc">
-        <button
-          type="submit"
+        <center>
+          <h1>Templates</h1>
+        </center>
+        <hr />
+        <div
           style={{
-            backgroundColor: "#336699",
-            width: "150px",
-            height: "40px",
-            fontWeight: "bold",
+            marginLeft: "80px",
           }}
         >
-          UPLOAD
-        </button>
-      </Link>
-      <br />
-      <br />
+          <u>
+            <h3>Proposal Document</h3>
+          </u>
+          <br />
+          <Link to="/uploaddoc">
+            <button
+              type="submit"
+              style={{
+                backgroundColor: "#336699",
+                width: "150px",
+                height: "40px",
+                fontWeight: "bold",
+              }}
+            >
+              UPLOAD
+            </button>
+          </Link>
+          <br />
+          <br />
 
-      <u>
-        <h3>Topic Assessment Form</h3>
-      </u>
-      <br />
-      <Link to="/uploaddoc">
-        <button
-          type="submit"
-          style={{
-            backgroundColor: "#336699",
-            width: "150px",
-            height: "40px",
-            fontWeight: "bold",
-          }}
-        >
-          UPLOAD
-        </button>
-      </Link>
-      <br />
-      <br />
+          <u>
+            <h3>Topic Assessment Form</h3>
+          </u>
+          <br />
+          <Link to="/uploaddoc">
+            <button
+              type="submit"
+              style={{
+                backgroundColor: "#336699",
+                width: "150px",
+                height: "40px",
+                fontWeight: "bold",
+              }}
+            >
+              UPLOAD
+            </button>
+          </Link>
+          <br />
+          <br />
 
-      <u>
-        <h3>Presentation Slides</h3>
-      </u>
-      <br />
-      <Link to="/uploaddoc">
-        <button
-          type="submit"
-          style={{
-            backgroundColor: "#336699",
-            width: "150px",
-            height: "40px",
-            fontWeight: "bold",
-          }}
-        >
-          UPLOAD
-        </button>
-      </Link>
-      <br />
-      <br />
+          <u>
+            <h3>Presentation Slides</h3>
+          </u>
+          <br />
+          <Link to="/uploaddoc">
+            <button
+              type="submit"
+              style={{
+                backgroundColor: "#336699",
+                width: "150px",
+                height: "40px",
+                fontWeight: "bold",
+              }}
+            >
+              UPLOAD
+            </button>
+          </Link>
+          <br />
+          <br />
 
-      <u>
-        <h3>Final Thesis</h3>
-      </u>
-      <br />
-      <Link to="/uploaddoc">
-        <button
-          type="submit"
-          style={{
-            backgroundColor: "#336699",
-            width: "150px",
-            height: "40px",
-            fontWeight: "bold",
-          }}
-        >
-          UPLOAD
-        </button>
-      </Link>
-      <br />
-      <br />
+          <u>
+            <h3>Final Thesis</h3>
+          </u>
+          <br />
+          <Link to="/uploaddoc">
+            <button
+              type="submit"
+              style={{
+                backgroundColor: "#336699",
+                width: "150px",
+                height: "40px",
+                fontWeight: "bold",
+              }}
+            >
+              UPLOAD
+            </button>
+          </Link>
+          <br />
+          <br />
 
-      <br />
-    </div>
+          <br />
+        </div>
+      </div>
     </div>
   );
 };
